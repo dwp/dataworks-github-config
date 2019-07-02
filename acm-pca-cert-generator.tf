4f18d8b2e20e91960ae10488ba80d6389b1b0bf6
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2019-07-02 10:42:52 +0100

    add acm-pca-cert-generator
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

---
+++ acm-pca-cert-generator.tf
@@ -0,0 +1,29 @@
+resource "github_repository" "acm-pca-cert-generator" {
+  name        = "acm-pca-cert-generator"
+  description = "Automatic creation of TLS certs generated with AWS' ACM PCA service"
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+}
+
+resource "github_team_repository" "acm-pca-cert-generator-dataworks" {
+  repository = "${github_repository.acm-pca-cert-generator.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "acm-pca-cert-generator-master" {
+  branch         = "${github_repository.acm-pca-cert-generator.default_branch}"
+  repository     = "${github_repository.acm-pca-cert-generator.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
