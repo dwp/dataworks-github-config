e3bacfd9db6ab458767d3dd2780a725bda027b02
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2019-07-05 10:20:09 +0100

    add data-key-service
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

---
+++ data-key-service.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "data-key-service" {
+  name        = "data-key-service"
+  description = "A service to assist with the generating and decrypting of data keys, backed by either AWS KMS or AWS CloudHSM v2"
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "data-key-service-dataworks" {
+  repository = "${github_repository.data-key-service.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "data-key-service-master" {
+  branch         = "${github_repository.data-key-service.default_branch}"
+  repository     = "${github_repository.data-key-service.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
