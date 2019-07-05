7aadced50351838f618aa5dc515dfc12766c79b8
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2019-07-05 16:00:51 +0100

    add kafka-to-hbase
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

---
+++ kafka-to-hbase.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "kafka-to-hbase" {
+  name        = "kafka-to-hbase"
+  description = "Simple shovel utility to move messages from a Kafka topic to Hbase with versioning"
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "kafka-to-hbase-dataworks" {
+  repository = "${github_repository.kafka-to-hbase.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "kafka-to-hbase-master" {
+  branch         = "${github_repository.kafka-to-hbase.default_branch}"
+  repository     = "${github_repository.kafka-to-hbase.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
