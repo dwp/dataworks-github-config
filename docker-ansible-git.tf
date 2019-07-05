ae8ef74f1aa182000469ad95736dd804580104aa
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2019-07-05 10:26:27 +0100

    add docker-ansible-git
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

---
+++ docker-ansible-git.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "docker-ansible-git" {
+  name        = "docker-ansible-git"
+  description = "Docker CentOS image with Ansible and Git. Used in CI pipeline to install required Ansible roles."
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "docker-ansible-git-dataworks" {
+  repository = "${github_repository.docker-ansible-git.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "docker-ansible-git-master" {
+  branch         = "${github_repository.docker-ansible-git.default_branch}"
+  repository     = "${github_repository.docker-ansible-git.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
