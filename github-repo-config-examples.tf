859537e20c9ace48d9d35161066db99a7095b5e8
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2019-07-05 15:49:50 +0100

    add github-repo-config-examples
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

---
+++ github-repo-config-examples.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "github-repo-config-examples" {
+  name        = "github-repo-config-examples"
+  description = "Examples and templates for configuring GitHub repositories using Terraform"
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "github-repo-config-examples-dataworks" {
+  repository = "${github_repository.github-repo-config-examples.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "github-repo-config-examples-master" {
+  branch         = "${github_repository.github-repo-config-examples.default_branch}"
+  repository     = "${github_repository.github-repo-config-examples.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
