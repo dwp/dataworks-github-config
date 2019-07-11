dc0eacda44bbb068e239392d05551e63959a7d93
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2019-07-11 10:45:34 +0100

    add registry-image-resource
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

---
+++ registry-image-resource.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "registry-image-resource" {
+  name        = "registry-image-resource"
+  description = "a resource for images in a Docker registry"
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "registry-image-resource-dataworks" {
+  repository = "${github_repository.registry-image-resource.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "registry-image-resource-master" {
+  branch         = "${github_repository.registry-image-resource.default_branch}"
+  repository     = "${github_repository.registry-image-resource.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
