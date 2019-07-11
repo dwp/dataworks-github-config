951255cf7ac302bc27df4d912a8fd62849577144
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2019-07-11 11:21:42 +0100

    add terraform-aws-vpc
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

---
+++ terraform-aws-vpc.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "terraform-aws-vpc" {
+  name        = "terraform-aws-vpc"
+  description = "A Terraform module to create an AWS VPC with consistent features"
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "terraform-aws-vpc-dataworks" {
+  repository = "${github_repository.terraform-aws-vpc.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "terraform-aws-vpc-master" {
+  branch         = "${github_repository.terraform-aws-vpc.default_branch}"
+  repository     = "${github_repository.terraform-aws-vpc.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
