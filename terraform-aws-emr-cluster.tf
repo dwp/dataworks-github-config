41285ca86e8660da3960d9a75a2fde08f825f507
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2019-07-11 10:52:09 +0100

    add terraform-aws-emr-cluster
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

---
+++ terraform-aws-emr-cluster.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "terraform-aws-emr-cluster" {
+  name        = "terraform-aws-emr-cluster"
+  description = "A Terraform module to create an Amazon Web Services (AWS) Elastic MapReduce (EMR) cluster."
+
+  allow_merge_commit = false
+  default_branch     = "develop"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "terraform-aws-emr-cluster-dataworks" {
+  repository = "${github_repository.terraform-aws-emr-cluster.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "terraform-aws-emr-cluster-master" {
+  branch         = "${github_repository.terraform-aws-emr-cluster.default_branch}"
+  repository     = "${github_repository.terraform-aws-emr-cluster.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
