fe9b4cf359f6ab408c5a5f8a28c6e7a7ae4e73de
Daniel Chicot <dan.chicot@gmail.com>
2019-07-12 10:34:17 +0100

    Change to effect hbase-crown-export -> hbase-to-mongo-export module rename.

---
+++ hbase-to-mongo-export.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "hbase-to-mongo-export" {
+  name        = "hbase-to-mongo-export"
+  description = ""
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "hbase-to-mongo-export-dataworks" {
+  repository = "${github_repository.hbase-to-mongo-export.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "hbase-to-mongo-export-master" {
+  branch         = "${github_repository.hbase-to-mongo-export.default_branch}"
+  repository     = "${github_repository.hbase-to-mongo-export.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
