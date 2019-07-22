203289572ab172f660fce22662ba457f0b672c61
Benjamin Sherwood <benjamin.sherwood@dwp.gov.uk>
2019-07-22 14:43:20 +0100

    Removed "hsm" from name

---
+++ docker-aws-openssl.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "docker-aws-openssl" {
+  name        = "docker-aws-openssl"
+  description = "Docker alpine image with AWS CLI and OpenSSL"
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "docker-aws-openssl-dataworks" {
+  repository = "${github_repository.docker-aws-openssl.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "docker-aws-openssl-master" {
+  branch         = "${github_repository.docker-aws-openssl.default_branch}"
+  repository     = "${github_repository.docker-aws-openssl.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
