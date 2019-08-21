6d047b05a16196f5a1cad9ec81779760295f7fc5
benjaminsherwood <benjamin.sherwood@dmwgroup.co.uk>
2019-08-21 12:10:10 +0100

    Added mock-nifi repository config (#9)

---
+++ mock-nifi.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "mock-nifi" {
+  name        = "mock-nifi"
+  description = "Repository for the mock-nifi application"
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "mock-nifi-dataworks" {
+  repository = "${github_repository.mock-nifi.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "mock-nifi-master" {
+  branch         = "${github_repository.mock-nifi.default_branch}"
+  repository     = "${github_repository.mock-nifi.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
