84137442c1e13816013f5ba6d51f8092c506f8c5
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2019-09-23 09:28:02 +0100

    add kafka-to-s3 repo (#11)
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

---
+++ kafka-to-s3.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "kafka-to-s3" {
+  name        = "kafka-to-s3"
+  description = "Read from Kafka topic and store in S3"
+
+  allow_merge_commit = false
+  auto_init          = true
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "kafka-to-s3-dataworks" {
+  repository = "${github_repository.kafka-to-s3.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "kafka-to-s3-master" {
+  branch         = "${github_repository.kafka-to-s3.default_branch}"
+  repository     = "${github_repository.kafka-to-s3.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
