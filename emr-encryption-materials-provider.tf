3dfa171fd3ce0fdb03bf63cedf9111c1e1cc48a2
Berrylands <jason.a.edge@gmail.com>
2019-09-26 21:27:01 +0100

    add emr-encryption-materials-provider repo

---
+++ emr-encryption-materials-provider.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "emr-encryption-materials-provider" {
+  name        = "emr-encryption-materials-provider"
+  description = "An EMR Security Configuration plugin implementing transparent client-side encryption and decryption between EMR and data persisted in S3 (via EMRFS)"
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "emr-encryption-materials-provider-dataworks" {
+  repository = "${github_repository.emr-encryption-materials-provider.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "emr-encryption-materials-provider-master" {
+  branch         = "${github_repository.emr-encryption-materials-provider.default_branch}"
+  repository     = "${github_repository.emr-encryption-materials-provider.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
