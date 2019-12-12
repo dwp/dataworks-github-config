ea01fa835462a9495d1633dce0df927553a1552e
Ben F <ben.fletcher@dmwgroup.co.uk>
2019-12-12 14:19:46 +0000

    Added new repo

---
+++ emr-cluster-broker.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "emr-cluster-broker" {
+  name        = "emr-cluster-broker"
+  description = "A lightweight API to passthrough requests to AWS SDK and create EMR clusters"
+
+  allow_merge_commit = false
+  auto_init          = true
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "emr-cluster-broker-dataworks" {
+  repository = "${github_repository.emr-cluster-broker.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "admin"
+}
+
+resource "github_branch_protection" "emr-cluster-broker-master" {
+  branch         = "${github_repository.emr-cluster-broker.default_branch}"
+  repository     = "${github_repository.emr-cluster-broker.name}"
+  enforce_admins = true
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
