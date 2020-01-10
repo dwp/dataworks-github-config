b8b91649e5286c25957e72805ba5a3b69ffced60
Joe Stringer <joe.stringer@infinityworks.com>
2020-01-10 13:58:39 +0000

    Adding AWS-Concourse

---
+++ aws-concourse.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "aws-concourse" {
+  name        = "aws-concourse"
+  description = "An AWS based Concourse platform"
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "aws-concourse-dataworks" {
+  repository = "${github_repository.aws-concourse.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "aws-concourse-master" {
+  branch         = "${github_repository.aws-concourse.default_branch}"
+  repository     = "${github_repository.aws-concourse.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
