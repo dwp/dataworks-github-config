a73487bbb4319c1d7b066c2b4b0868d3859f0aa1
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2020-01-15 12:08:00 +0000

    import hbase-retriever
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

---
+++ hbase-retriever.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "hbase-retriever" {
+  name        = "hbase-retriever"
+  description = "Retrieves records from HBase"
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "hbase-retriever-dataworks" {
+  repository = "${github_repository.hbase-retriever.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "hbase-retriever-master" {
+  branch         = "${github_repository.hbase-retriever.default_branch}"
+  repository     = "${github_repository.hbase-retriever.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
