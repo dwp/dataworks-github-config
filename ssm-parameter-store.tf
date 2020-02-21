660a4f8f1fe80a63eec8539f1ba83de9e23f2c13
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-02-21 12:08:57 +0000

    Creating repo for ssm-parameter-store content

---
+++ ssm-parameter-store.tf
@@ -0,0 +1,34 @@
+resource "github_repository" "ssm-parameter-store" {
+  name        = "ssm-parameter-store"
+  description = "Docker container with JQ and Curl"
+  auto_init   = true
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "ssm-parameter-store-dataworks" {
+  repository = "${github_repository.ssm-parameter-store.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "ssm-parameter-store-master" {
+  branch         = "${github_repository.ssm-parameter-store.default_branch}"
+  repository     = "${github_repository.ssm-parameter-store.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
