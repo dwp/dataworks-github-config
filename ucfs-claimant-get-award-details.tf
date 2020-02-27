baaac1d80c2ad05d0a5dd34052aa196c21699b88
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2020-02-27 15:55:39 +0000

    add ucfs-claimant-get-award-details repo
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

---
+++ ucfs-claimant-get-award-details.tf
@@ -0,0 +1,34 @@
+resource "github_repository" "ucfs-claimant-get-award-details" {
+  name        = "ucfs-claimant-get-award-details"
+  description = "Get Award Details Lambda code for UCFS Claimant API endpoint"
+  auto_init   = true
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "ucfs-claimant-get-award-details_dataworks" {
+  repository = "${github_repository.ucfs-claimant-get-award-details.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "ucfs-claimant-get-award-details_master" {
+  branch         = "${github_repository.ucfs-claimant-get-award-details.default_branch}"
+  repository     = "${github_repository.ucfs-claimant-get-award-details.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
