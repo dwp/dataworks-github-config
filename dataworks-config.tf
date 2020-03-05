9b87f11f686cdad664f9b8a78f171bf631d9e95d
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-03-05 11:56:26 +0000

    Created config repo for secrets (#52)
    * Created config repo for secrets
    
    * Fixed poor formatting
    
    * Created aws-analytical-dataset-generation repo (#53)
    
    * Created aws-analytical-dataset-generation repo
    
    * Fixed poor formatting
    
    * Fixed poor formatting
    
    * Created config repo for secrets
    
    * Fixed poor formatting

---
+++ dataworks-config.tf
@@ -0,0 +1,34 @@
+resource "github_repository" "dataworks-config" {
+  name        = "dataworks-config"
+  description = "Repo for storing SSM Parameter Store content"
+  auto_init   = true
+
+  allow_merge_commit = false
+  default_branch     = "master"
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "dataworks-config_dataworks" {
+  repository = "${github_repository.dataworks-config.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "dataworks-config_master" {
+  branch         = "${github_repository.dataworks-config.default_branch}"
+  repository     = "${github_repository.dataworks-config.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
