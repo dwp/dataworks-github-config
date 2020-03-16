fa1380f5c9a4ce208c5a56cc1b9620dabf5f0a51
Dmitrijs Kovalenko <dmitrjis.kovalenko@gmail.com>
2020-03-16 14:48:35 +0000

    Create a repo for DW-3669 - manage mysql users

---
+++ manage-mysql-user.tf
@@ -0,0 +1,34 @@
+resource "github_repository" "manage-mysql-user" {
+  name        = "manage-mysql-user"
+  description = "Manages MySQL users"
+  auto_init   = true
+
+  allow_merge_commit = false
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "manage-mysql-user-dataworks" {
+  repository = "${github_repository.manage-mysql-user.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "manage-mysql-user-master" {
+  branch         = "${github_repository.manage-mysql-user.default_branch}"
+  repository     = "${github_repository.manage-mysql-user.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+    contexts = ["concourse-ci/example-pr"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
