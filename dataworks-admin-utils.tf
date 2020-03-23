e1b1513a93bb4c8575108e95216c52b71d45d892
mark matthews <mark.matthews1@dwp.gov.uk>
2020-03-23 12:46:00 +0000

    DW-3689 create dataworks-admin-utils repo

---
+++ dataworks-admin-utils.tf
@@ -0,0 +1,34 @@
+resource "github_repository" "dataworks-admin-utils" {
+  name        = "dataworks-admin-utils"
+  description = "Contains dataworks administrative utilities"
+  auto_init   = true
+
+  allow_merge_commit = false
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "dataworks_admin_utils_dataworks" {
+  repository = "${github_repository.dataworks-admin-utils.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "dataworks_admin_utils_master" {
+  branch         = "${github_repository.dataworks-admin-utils.default_branch}"
+  repository     = "${github_repository.dataworks-admin-utils.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+    contexts = ["concourse-ci/dataworks-admin-utils-pr"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
