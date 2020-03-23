c254cc0427c7386a707d1d9fb56b93ba07175747
caseyr232 <c@sey.org.uk>
2020-03-23 15:46:07 +0000

    Dw 3705 hardened chrome (#69)
    * DW-3705: Create hardened Chrome containers
    
    * DW-3705: Create hardened Chrome containers

---
+++ hardened-guac-chrome.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "hardened-guac-chrome" {
+  name        = "hardened-guac-chrome"
+  description = "The hardened Guacamole Chrome container"
+
+  allow_merge_commit = false
+  has_issues         = true
+  auto_init          = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "hardened-guac-chrome-dataworks" {
+  repository = "${github_repository.hardened-guac-chrome.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "hardened-guac-chrome-master" {
+  branch         = "${github_repository.hardened-guac-chrome.default_branch}"
+  repository     = "${github_repository.hardened-guac-chrome.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
