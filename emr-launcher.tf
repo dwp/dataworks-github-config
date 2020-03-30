3031be6bae32b576e5e122a6838a7906ab75ada6
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-03-30 10:59:38 +0100

    EMR Launcher Repo (#73)

---
+++ emr-launcher.tf
@@ -0,0 +1,34 @@
+resource "github_repository" "emr-launcher" {
+  name        = "emr-launcher"
+  description = "Lambda based EMR Cluster Launcher"
+  auto_init   = true
+
+  allow_merge_commit = false
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "emr-launcher_dataworks" {
+  repository = "${github_repository.emr-launcher.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "emr-launcher_master" {
+  branch         = "${github_repository.emr-launcher.default_branch}"
+  repository     = "${github_repository.emr-launcher.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+    # The contexts line should only be kept for Terraform repos.
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
