31e2f2cf541df178c6071f7335655f9a31cd0492
ian.utley <ian.utley@infinityworks.com>
2020-04-03 16:12:14 +0100

    Intial terraform for cognito-guacamole-extension

---
+++ cognito-guacamole-extension.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "cognito-guacamole-extension" {
+  name        = "cognito-guacamole-extension"
+  description = "Integration between cognito and Apache Guacamole"
+  auto_init   = true
+
+  allow_merge_commit = false
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "cognito-guacamole-extension-dataworks" {
+  repository = "${github_repository.cognito-guacamole-extension.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "cognito-guacamole-extension-master" {
+  branch         = "${github_repository.cognito-guacamole-extension.default_branch}"
+  repository     = "${github_repository.cognito-guacamole-extension.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
