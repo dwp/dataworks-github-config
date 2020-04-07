214567efc972200ca1ffd1923c1fc965cc1db0cb
Ben F <benj-fletch@users.noreply.github.com>
2020-04-07 14:02:27 +0100

    DW-3790 - New tactical repo to create AWS users

---
+++ tactical-aws-user-creation.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "tactical-aws-user-creation" {
+  name        = "tactical-aws-user-creation"
+  description = "Temporary app to create and assign MFA software token code to a user in AWS cognito."
+  auto_init   = true
+
+  allow_merge_commit = false
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "tactical-aws-user-creation-dataworks" {
+  repository = "${github_repository.tactical-aws-user-creation.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "example_master" {
+  branch         = "${github_repository.tactical-aws-user-creation.default_branch}"
+  repository     = "${github_repository.tactical-aws-user-creation.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
