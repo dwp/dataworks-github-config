6df730043553a0c233dd5cb19b665035f86b933c
draagc-iw <55280269+draagc-iw@users.noreply.github.com>
2020-04-24 09:21:08 +0100

    Add GitHub actions secrets (#85)
    * Add GH Actions secrets to terraform
    
    * Grab new secrets from Vault when running terraform apply

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "dockerhub_username" {
+    type = "string"
+    description = "DockerHub Username to provide to GitHub actions"
+}
+
+variable "dockerhub_password" {
+    type = "string"
+    description = "DockerHub Password or Access Token to provide to GitHub actions"
+}
+
+variable "snyk_token" {
+    type = "string"
+    description = "Snyk Token to provide to GitHub Actions"
+}
