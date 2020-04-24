298e04b05942f4979e3a69b284ec14516313a0a3
Daniel Hill <dan@mamu.co>
2020-04-24 14:14:38 +0100

    add ucfs-claimant-load-data repo (#84)
    * add ucfs-claimant-load-data repo
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>
    
    * rename to ucfs-claimant-api-load-data
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

---
+++ ucfs-claimant-load-data.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "ucfs-claimant-api-load-data" {
+  name        = "ucfs-claimant-api-load-data"
+  description = "Python Lambda to orchestrate loading data into RDS from S3 for UCFS Claimant API service"
+  auto_init   = true
+
+  allow_merge_commit = false
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "ucfs-claimant-api-load-data_dataworks" {
+  repository = "${github_repository.ucfs-claimant-api-load-data.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "ucfs-claimant-api-load-data_master" {
+  branch         = "${github_repository.ucfs-claimant-api-load-data.default_branch}"
+  repository     = "${github_repository.ucfs-claimant-api-load-data.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
