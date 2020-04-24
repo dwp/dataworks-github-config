b44eb3ae40a269adb163494b29c86eb472439367
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2020-04-24 13:46:05 +0100

    rename to ucfs-claimant-api-load-data
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

--- ucfs-claimant-load-data.tf
+++ ucfs-claimant-load-data.tf
@@ -1,5 +1,5 @@
-resource "github_repository" "ucfs-claimant-load-data" {
-  name        = "ucfs-claimant-load-data"
+resource "github_repository" "ucfs-claimant-api-load-data" {
+  name        = "ucfs-claimant-api-load-data"
   description = "Python Lambda to orchestrate loading data into RDS from S3 for UCFS Claimant API service"
   auto_init   = true
 
@@ -11,15 +11,15 @@ resource "github_repository" "ucfs-claimant-load-data" {
   }
 }
 
-resource "github_team_repository" "ucfs-claimant-load-data_dataworks" {
-  repository = "${github_repository.ucfs-claimant-load-data.name}"
+resource "github_team_repository" "ucfs-claimant-api-load-data_dataworks" {
+  repository = "${github_repository.ucfs-claimant-api-load-data.name}"
   team_id    = "${github_team.dataworks.id}"
   permission = "push"
 }
 
-resource "github_branch_protection" "ucfs-claimant-load-data_master" {
-  branch         = "${github_repository.ucfs-claimant-load-data.default_branch}"
-  repository     = "${github_repository.ucfs-claimant-load-data.name}"
+resource "github_branch_protection" "ucfs-claimant-api-load-data_master" {
+  branch         = "${github_repository.ucfs-claimant-api-load-data.default_branch}"
+  repository     = "${github_repository.ucfs-claimant-api-load-data.name}"
   enforce_admins = false
 
   required_status_checks {
