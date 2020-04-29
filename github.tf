21925e220198f567aeb5b8fa41c67997944a6bc1
Matthew Burgess <549318+mattburgess@users.noreply.github.com>
2020-04-29 11:58:31 +0100

    DW-3874: Add concourse webhooks (#93)

--- github.tf
+++ github.tf
@@ -9,4 +9,5 @@ variable github_organization {
 provider "github" {
   token        = "${var.github_token}"
   organization = "${var.github_organization}"
+  version      = "~> 2.6.1"
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,19 @@
 variable "dockerhub_username" {
-    type = "string"
-    description = "DockerHub Username to provide to GitHub actions"
+  type        = "string"
+  description = "DockerHub Username to provide to GitHub actions"
 }
 
 variable "dockerhub_password" {
-    type = "string"
-    description = "DockerHub Password or Access Token to provide to GitHub actions"
+  type        = "string"
+  description = "DockerHub Password or Access Token to provide to GitHub actions"
 }
 
 variable "snyk_token" {
-    type = "string"
-    description = "Snyk Token to provide to GitHub Actions"
+  type        = "string"
+  description = "Snyk Token to provide to GitHub Actions"
+}
+
+variable "github_webhook_token" {
+  type        = "string"
+  description = "GitHub token to auth with CI"
 }
