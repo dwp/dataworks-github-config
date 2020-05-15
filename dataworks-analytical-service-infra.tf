d3c6b04d8413b72ad4c704a84aecaa3ea15f2526
Ben F <benj-fletch@users.noreply.github.com>
2020-05-15 07:14:23 +0100

    DW-4056 add dataworks-analytical-service-infra (#102)

---
+++ dataworks-analytical-service-infra.tf
@@ -0,0 +1,34 @@
+resource "github_repository" "dataworks-analytical-service-infra" {
+  name        = "dataworks-analytical-service-infra"
+  description = "Terraform for analytical services infrastructure"
+  auto_init   = true
+
+  allow_merge_commit = false
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "dataworks-analytical-service-infra_dataworks" {
+  repository = "${github_repository.dataworks-analytical-service-infra.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "dataworks-analytical-service-infra_master" {
+  branch         = "${github_repository.dataworks-analytical-service-infra.default_branch}"
+  repository     = "${github_repository.dataworks-analytical-service-infra.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+    contexts = ["concourse-ci/dataworks-analytical-service-infra-pr"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
