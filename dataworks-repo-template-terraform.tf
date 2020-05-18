ca5a563ea9c68b37cd21f1107ce1ec851c54aa22
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-05-18 16:01:28 +0100

    _'s and -'s in the right places
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- dataworks-repo-template-terraform.tf
+++ dataworks-repo-template-terraform.tf
@@ -1,4 +1,4 @@
-resource "github_repository" "dataworks-repo-template-terraform" {
+resource "github_repository" "dataworks_repo_template_terraform" {
   name        = "dataworks-repo-template-terraform"
   description = "Template Terraform repository for DataWorks GitHub"
   auto_init   = true
@@ -12,15 +12,15 @@ resource "github_repository" "dataworks-repo-template-terraform" {
   }
 }
 
-resource "github_team_repository" "dataworks-repo-template-terraform_dataworks" {
-  repository = "${github_repository.dataworks-repo-template-terraform.name}"
+resource "github_team_repository" "dataworks_repo_template_terraform_dataworks" {
+  repository = "${github_repository.dataworks_repo_template_terraform.name}"
   team_id    = "${github_team.dataworks.id}"
   permission = "push"
 }
 
-resource "github_branch_protection" "dataworks-repo-template-terraform_master" {
-  branch         = "${github_repository.dataworks-repo-template-terraform.default_branch}"
-  repository     = "${github_repository.dataworks-repo-template-terraform.name}"
+resource "github_branch_protection" "dataworks_repo_template_terraform_master" {
+  branch         = "${github_repository.dataworks_repo_template_terraform.default_branch}"
+  repository     = "${github_repository.dataworks_repo_template_terraform.name}"
   enforce_admins = false
 
   required_status_checks {
