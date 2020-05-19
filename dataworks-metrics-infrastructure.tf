c49fb9ae39e47b458ad86d0e5eba6d9ecd8751b2
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-05-19 12:01:59 +0100

    Renamed repo as per request
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- dataworks-metrics-infrastructure.tf
+++ dataworks-metrics-infrastructure.tf
@@ -1,5 +1,5 @@
-resource "github_repository" "aws-metrics-infrastructure" {
-  name        = "aws-metrics-infrastructure"
+resource "github_repository" "dataworks_metrics_infrastructure" {
+  name        = "dataworks-metrics-infrastructure"
   description = "Repo for storing the Metrics Infrastructure to be used in AWS"
   auto_init   = true
 
@@ -11,15 +11,15 @@ resource "github_repository" "aws-metrics-infrastructure" {
   }
 }
 
-resource "github_team_repository" "aws-metrics-infrastructure_dataworks" {
-  repository = "${github_repository.aws-metrics-infrastructure.name}"
+resource "github_team_repository" "dataworks_metrics_infrastructure_dataworks" {
+  repository = "${github_repository.dataworks_metrics_infrastructure.name}"
   team_id    = "${github_team.dataworks.id}"
   permission = "push"
 }
 
-resource "github_branch_protection" "aws-metrics-infrastructure_master" {
-  branch         = "${github_repository.aws-metrics-infrastructure.default_branch}"
-  repository     = "${github_repository.aws-metrics-infrastructure.name}"
+resource "github_branch_protection" "dataworks_metrics_infrastructure_master" {
+  branch         = "${github_repository.dataworks_metrics_infrastructure.default_branch}"
+  repository     = "${github_repository.dataworks_metrics_infrastructure.name}"
   enforce_admins = false
 
   required_status_checks {
