790dbe33862dc1ede6e385b76bdbbf2798b360d6
MattToczek <52243130+MattToczek@users.noreply.github.com>
2020-05-22 12:06:45 +0100

    adding custom-auth-lambda-triggers repo (#111)
    * adding custom-auth-lambda-triggers repo
    
    * renaming to be specific to the analytical env
    
    * renaming to be specific to the analytical env
    
    * adding Snyk token as env var
    
    * refactoring resource names to match other repos

---
+++ dataworks-analytical-custom-auth-flow.tf
@@ -0,0 +1,41 @@
+resource "github_repository" "dataworks-analytical-custom-auth-flow" {
+  name        = "dataworks-analytical-custom-auth-flow"
+  description = "Three lambdas to create a custom authentication flow in cognito for the analytical environment"
+  auto_init   = true
+
+  allow_merge_commit = false
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "dataworks-analytical-custom-auth-flow-dataworks" {
+  repository = "${github_repository.dataworks-analytical-custom-auth-flow.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "dataworks-analytical-custom-auth-flow-master" {
+  branch         = "${github_repository.dataworks-analytical-custom-auth-flow.default_branch}"
+  repository     = "${github_repository.dataworks-analytical-custom-auth-flow.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+    # The contexts line should only be kept for Terraform repos
+    contexts = ["concourse-ci/dataworks-analytical-custom-auth-flow-pr"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
+
+resource "github_actions_secret" "dataworks-analytical-custom-auth-flow-snyk-token" {
+  repository      = "${github_repository.dataworks-analytical-custom-auth-flow.name}"
+  secret_name     = "SNYK_TOKEN"
+  plaintext_value = "${var.snyk_token}"
+}
