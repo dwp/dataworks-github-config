1fffae9013f53f8a7e64c543e7c8459d3b85f035
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-05-28 13:30:46 +0100

    Adding repository for holding Git hooks (#115)

---
+++ dataworks-githooks.tf
@@ -0,0 +1,33 @@
+resource "github_repository" "dataworks_githooks" {
+  name        = "dataworks-githooks"
+  description = "Repo for holding DataWorks Git hooks"
+  auto_init   = true
+
+  allow_merge_commit = false
+  has_issues         = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "dataworks_githooks_dataworks" {
+  repository = "${github_repository.dataworks_githooks.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "dataworks_githooks_master" {
+  branch         = "${github_repository.dataworks_githooks.default_branch}"
+  repository     = "${github_repository.dataworks_githooks.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
