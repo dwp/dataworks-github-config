7472cb77fcfc958d65a1eab18afff3123d747219
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-06-05 11:08:01 +0100

    No need for IF statement as it always exists
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- initial-commit-tf.sh
+++ initial-commit-tf.sh
@@ -8,21 +8,15 @@ git config --global user.email "${GIT_EMAIL}"
 git clone https://github.com/dwp/$NEW_REPO_NAME
 cd $NEW_REPO_NAME
 
-if [ ! -z 'grep -rnw $TF_REPO_NAME ci' ]; then
-    git submodule add https://github.com/dwp/dataworks-githooks .githooks
-    make git-hooks
-    
-    find ci -type f -exec sed -i '' "s/$TF_REPO_NAME/$NEW_REPO_NAME/g" {} +
-    find terraform -type f -exec sed -i '' "s/$TF_REPO_NAME/$NEW_REPO_NAME/g" {} +
-    find aviator.yml -type f -exec sed -i '' "s/$TF_REPO_NAME/$NEW_REPO_NAME/g" {} +
-    
-    rm initial-commit.sh
-    
-    git add --all
-    git commit -m "Renamed pipeline to fit repository"
-    git push --quiet --set-upstream origin initial-commit
-    
-else
-    echo "Not required, or not a Terraform repository"
-    exit 1
-fi
+git submodule add https://github.com/dwp/dataworks-githooks .githooks
+make git-hooks
+
+find ci -type f -exec sed -i '' "s/$TF_REPO_NAME/$NEW_REPO_NAME/g" {} +
+find terraform -type f -exec sed -i '' "s/$TF_REPO_NAME/$NEW_REPO_NAME/g" {} +
+find aviator.yml -type f -exec sed -i '' "s/$TF_REPO_NAME/$NEW_REPO_NAME/g" {} +
+
+rm initial-commit.sh
+
+git add --all
+git commit -m "Renamed pipeline and Terraform to fit repository"
+git push --quiet --set-upstream origin initial-commit
