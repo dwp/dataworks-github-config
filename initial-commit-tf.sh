6f20fa9aa089fb6cf9359d3edbfd96504f3c3fc3
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-06-08 11:28:32 +0100

    Added secription to the samples, fixed init to run on linux container
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- initial-commit-tf.sh
+++ initial-commit-tf.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 TF_REPO_NAME=dataworks-repo-template-terraform
 NEW_REPO_NAME=$1
+REPO_DESCRIPTION="$2"
 
 git config --global user.name "${GIT_USERNAME}"
 git config --global user.email "${GIT_EMAIL}"
@@ -9,12 +10,12 @@ git clone https://github.com/dwp/$NEW_REPO_NAME
 cd $NEW_REPO_NAME
 
 git submodule add https://github.com/dwp/dataworks-githooks .githooks
-make git-hooks
 
-find ci -type f -exec sed -i '' "s/$TF_REPO_NAME/$NEW_REPO_NAME/g" {} +
-find terraform -type f -exec sed -i '' "s/$TF_REPO_NAME/$NEW_REPO_NAME/g" {} +
-find aviator.yml -type f -exec sed -i '' "s/$TF_REPO_NAME/$NEW_REPO_NAME/g" {} +
-find README.md -type -f -exec sed -i '' "s/#\ $TF_REPO_NAME/#\ $NEW_REPO_NAME/" {} +
+find ci -type f -exec sed -i "s/$TF_REPO_NAME/$NEW_REPO_NAME/g" {} +
+find terraform -type f -exec sed -i "s/$TF_REPO_NAME/$NEW_REPO_NAME/g" {} +
+find aviator.yml -type f -exec sed -i "s/$TF_REPO_NAME/$NEW_REPO_NAME/g" {} +
+find README.md -type f -exec sed -i "s/#\ $TF_REPO_NAME/#\ $NEW_REPO_NAME/g" {} +
+find README.md -type f -exec sed -i "s/##\ Description/##\ ${REPO_DESCRIPTION}/g" {} +
 
 rm initial-commit.sh
 
--- initial-commit.sh
+++ initial-commit.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 REPO_NAME=dataworks-repo-template
 NEW_REPO_NAME=$1
+REPO_DESCRIPTION=$2
 
 git config --global user.name "${GIT_USERNAME}"
 git config --global user.email "${GIT_EMAIL}"
 
 git clone https://github.com/dwp/$NEW_REPO_NAME
 cd $NEW_REPO_NAME
-git submodule add https://github.com/dwp/dataworks-githooks .githooks
-make git-hooks
 
-find README.md -type -f -exec sed -i '' "s/#\ $REPO_NAME/#\ $NEW_REPO_NAME/" {} +
+git submodule add https://github.com/dwp/dataworks-githooks .githooks
+
+find README.md -type f -exec sed -i "s/#\ $REPO_NAME/#\ $NEW_REPO_NAME/" {} +
+find README.md -type f -exec sed -i "s/##\ Description/##\ ${REPO_DESCRIPTION}/g" {} +
 
 rm initial-commit.sh
 
