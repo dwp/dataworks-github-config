6df7469a6ecd99d5022aed3302e6a2d4e9228116
Daniel Hill <dan@mamu.co>
2020-06-19 10:30:32 +0100

    change secret names so not prefixed with `GITHUB_`
    > Secret names must not start with the `GITHUB_` prefix.
    https://help.github.com/en/actions/configuring-and-managing-workflows/creating-and-storing-encrypted-secrets

--- kafka-producer.tf
+++ kafka-producer.tf
@@ -35,18 +35,18 @@ resource "github_branch_protection" "kafka-producer-master" {
 
 resource "github_actions_secret" "kafka-producer_github_email" {
   repository      = "${github_repository.kafka-producer.name}"
-  secret_name     = "GITHUB_EMAIL"
+  secret_name     = "CI_GITHUB_EMAIL"
   plaintext_value = "${var.github_email}"
 }
 
 resource "github_actions_secret" "kafka-producer_github_username" {
   repository      = "${github_repository.kafka-producer.name}"
-  secret_name     = "GITHUB_USERNAME"
+  secret_name     = "CI_GITHUB_USERNAME"
   plaintext_value = "${var.github_username}"
 }
 
 resource "github_actions_secret" "kafka-producer_github_token" {
   repository      = "${github_repository.kafka-producer.name}"
-  secret_name     = "GITHUB_TOKEN"
+  secret_name     = "CI_GITHUB_TOKEN"
   plaintext_value = "${var.github_token}"
 }
