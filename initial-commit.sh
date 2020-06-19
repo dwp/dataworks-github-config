daf2b32ed3060848b1e6bacc1a2c69eabd1045be
Dmitrijs Kovalenko <dmitrjiskovalenko@digital.uc.dwp.gov.uk>
2020-06-19 20:55:48 +0100

    Modify CI jobs in TF template repo on initial commit

--- initial-commit.sh
+++ initial-commit.sh
@@ -17,6 +17,8 @@ find README.md -type f -exec sed -i "s/##\ Description/##\ ${REPO_DESCRIPTION}/g
 case "$REPO_NAME" in
     *-terraform)
         find ci -type f -exec sed -i "s/$REPO_NAME/$NEW_REPO_NAME/g" {} +
+        find ci -type f -exec sed -i s/"#ENABLE "/""/g {} +
+        find ci -type f -exec sed -i /"^.*#REMOVE.*"/d {} +
         find *.tf -type f -exec sed -i "s/$REPO_NAME/$NEW_REPO_NAME/g" {} +
         find aviator.yml -type f -exec sed -i "s/$REPO_NAME/$NEW_REPO_NAME/g" {} +
     ;;
