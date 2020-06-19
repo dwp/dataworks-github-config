7b01863fe13faf1b028f81434b0ed63cfba226cf
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-06-19 06:54:40 +0100

    Creating repo for Thanos Docker image
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

---
+++ docker-thanos.tf
@@ -0,0 +1,66 @@
+resource "github_repository" "docker_thanos" {
+  name             = "docker-thanos"
+  description      = "Rebuild of Thanos Docker image for DataWorks"
+  auto_init        = true
+
+  allow_merge_commit     = false
+  delete_branch_on_merge = true
+  has_issues             = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  template {
+    owner = "${var.github_organization}"
+    repository = "dataworks-repo-template-docker"
+  }
+}
+
+resource "github_team_repository" "docker_thanos_dataworks" {
+  repository = "${github_repository.docker_thanos.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "docker_thanos_master" {
+  branch         = "${github_repository.docker_thanos.default_branch}"
+  repository     = "${github_repository.docker_thanos.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
+
+resource "null_resource" "docker_thanos" {
+  triggers = {
+    repo = "${github_repository.docker_thanos.name}"
+  }
+  provisioner "local-exec" {
+    command = "./initial-commit.sh ${github_repository.docker_thanos.name} '${github_repository.docker_thanos.description}' ${github_repository.docker_thanos.template.0.repository}"
+  }
+}
+
+resource "github_actions_secret" "docker_thanos_dockerhub_password" {
+  repository      = "${github_repository.docker_thanos.name}"
+  secret_name     = "DOCKERHUB_PASSWORD"
+  plaintext_value = "${var.dockerhub_password}"
+}
+
+resource "github_actions_secret" "docker_thanos_dockerhub_username" {
+  repository      = "${github_repository.docker_thanos.name}"
+  secret_name     = "DOCKERHUB_USERNAME"
+  plaintext_value = "${var.dockerhub_username}"
+}
+
+resource "github_actions_secret" "docker_thanos_snyk_token" {
+  repository      = "${github_repository.docker_thanos.name}"
+  secret_name     = "SNYK_TOKEN"
+  plaintext_value = "${var.snyk_token}"
+}
