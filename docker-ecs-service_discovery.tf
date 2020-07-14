f3859afa1e3c39ae04b34bd420d03715a6e5ee15
iw-gtadam <61688288+iw-gtadam@users.noreply.github.com>
2020-07-14 15:29:31 +0100

    Adding repo to hold ECS Service Discovery Service (#184)

---
+++ docker-ecs-service_discovery.tf
@@ -0,0 +1,66 @@
+resource "github_repository" "docker_ecs_service_discovery" {
+  name             = "docker-ecs-service-discovery"
+  description      = "Repo for the Docker ECS service discovery service for use with Prometheus"
+  auto_init        = true
+
+  allow_merge_commit     = false
+  delete_branch_on_merge = true
+  has_issues             = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  template {
+    owner = "${var.github_organization}"
+    repository = "dataworks-repo-template-docker"
+  }
+}
+
+resource "github_team_repository" "docker_ecs_service_discovery_dataworks" {
+  repository = "${github_repository.docker_ecs_service_discovery.name}"
+  team_id    = "${github_team.dataworks.id}"
+  permission = "push"
+}
+
+resource "github_branch_protection" "docker_ecs_service_discovery_master" {
+  branch         = "${github_repository.docker_ecs_service_discovery.default_branch}"
+  repository     = "${github_repository.docker_ecs_service_discovery.name}"
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
+
+resource "null_resource" "docker_ecs_service_discovery" {
+  triggers = {
+    repo = "${github_repository.docker_ecs_service_discovery.name}"
+  }
+  provisioner "local-exec" {
+    command = "./initial-commit.sh ${github_repository.docker_ecs_service_discovery.name} '${github_repository.docker_ecs_service_discovery.description}' ${github_repository.docker_ecs_service_discovery.template.0.repository}"
+  }
+}
+
+resource "github_actions_secret" "docker_ecs_service_discovery_dockerhub_password" {
+  repository      = "${github_repository.docker_ecs_service_discovery.name}"
+  secret_name     = "DOCKERHUB_PASSWORD"
+  plaintext_value = "${var.dockerhub_password}"
+}
+
+resource "github_actions_secret" "docker_ecs_service_discovery_dockerhub_username" {
+  repository      = "${github_repository.docker_ecs_service_discovery.name}"
+  secret_name     = "DOCKERHUB_USERNAME"
+  plaintext_value = "${var.dockerhub_username}"
+}
+
+resource "github_actions_secret" "docker_ecs_service_discovery_snyk_token" {
+  repository      = "${github_repository.docker_ecs_service_discovery.name}"
+  secret_name     = "SNYK_TOKEN"
+  plaintext_value = "${var.snyk_token}"
+}
