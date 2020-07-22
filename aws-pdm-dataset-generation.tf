6e5c9cf0b3b461164e6ed685c580f9b9388c1d9f
Levan Kirvalidze <levanik@live.com>
2020-07-22 13:43:27 +0100

    changing job name to reflect new concourse names

--- aws-pdm-dataset-generation.tf
+++ aws-pdm-dataset-generation.tf
@@ -39,7 +39,7 @@ resource "github_repository_webhook" "aws-pdm-dataset-generation" {
   events     = ["push"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.aws-pdm-dataset-generation.name}/resources/${github_repository.aws-pdm-dataset-generation.name}/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/pdm-dataset-generation/resources/${github_repository.aws-pdm-dataset-generation.name}/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
@@ -49,7 +49,7 @@ resource "github_repository_webhook" "aws-pdm-dataset-generation_pr" {
   events     = ["pull_request"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.aws-pdm-dataset-generation.name}/resources/${github_repository.aws-pdm-dataset-generation.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/pdm-dataset-generation`/resources/${github_repository.aws-pdm-dataset-generation.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
