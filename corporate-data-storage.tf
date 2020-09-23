65b973f9a98d0c1a17849959f116f0d5704430b3
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2020-09-23 15:47:39 +0100

    Remove SDI repo and add HDL, CDL repos

---
+++ corporate-data-storage.tf
@@ -0,0 +1,48 @@
+resource "github_repository" "corporate_data_loader" {
+  name             = "corporate-data-loader"
+  description      = "corporate_data_loader"
+  auto_init        = false
+
+  allow_merge_commit     = false
+  delete_branch_on_merge = true
+  has_issues             = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  template {
+    owner = var.github_organization
+    repository = "dataworks-repo-template"
+  }
+}
+
+resource "github_team_repository" "corporate_data_loader_dataworks" {
+  repository = github_repository.corporate_data_loader.name
+  team_id    = github_team.dataworks.id
+  permission = "push"
+}
+
+resource "github_branch_protection" "corporate_data_loader_master" {
+  branch         = github_repository.corporate_data_loader.default_branch
+  repository     = github_repository.corporate_data_loader.name
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
+
+resource "null_resource" "corporate_data_loader" {
+  triggers = {
+    repo = github_repository.corporate_data_loader.name
+  }
+  provisioner "local-exec" {
+    command = "./initial-commit.sh ${github_repository.corporate_data_loader.name} '${github_repository.corporate_data_loader.description}' ${github_repository.corporate_data_loader.template.0.repository}"
+  }
+}
