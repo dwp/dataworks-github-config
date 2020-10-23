db7505619897353d780cae1f822067dec7f5fdcf
Sandeep Gopishetty <sandeep.gopishetty@dmwgroup.co.uk>
2020-10-23 09:34:18 +0100

    DW-5074 addressed PR comments

--- dataworks-aws-ucfs-stub.tf
+++ dataworks-aws-ucfs-stub.tf
@@ -1,7 +1,7 @@
-resource "github_repository" "dataworks-aws-ucfs-stub" {
-  name             = "dataworks-aws-ucfs-stub"
-  description      = "A repo for DataWorks AWS UCFS export server stub"
-  auto_init        = false
+resource "github_repository" "dataworks_aws_ucfs_stub" {
+  name        = "dataworks-aws-ucfs-stub"
+  description = "A repo for DataWorks AWS UCFS export server stub"
+  auto_init   = false
 
   allow_merge_commit     = false
   delete_branch_on_merge = true
@@ -13,24 +13,24 @@ resource "github_repository" "dataworks-aws-ucfs-stub" {
   }
 
   template {
-    owner = var.github_organization
+    owner      = var.github_organization
     repository = "dataworks-repo-template-terraform"
   }
 }
 
-resource "github_team_repository" "dataworks-aws-ucfs-stub_dataworks" {
-  repository = github_repository.dataworks-aws-ucfs-stub.name
+resource "github_team_repository" "dataworks_aws_ucfs_stub_dataworks" {
+  repository = github_repository.dataworks_aws_ucfs_stub.name
   team_id    = github_team.dataworks.id
   permission = "push"
 }
 
-resource "github_branch_protection" "dataworks-aws-ucfs-stub_master" {
-  branch         = github_repository.dataworks-aws-ucfs-stub.default_branch
-  repository     = github_repository.dataworks-aws-ucfs-stub.name
+resource "github_branch_protection" "dataworks_aws_ucfs_stub_master" {
+  branch         = github_repository.dataworks_aws_ucfs_stub.default_branch
+  repository     = github_repository.dataworks_aws_ucfs_stub.name
   enforce_admins = false
 
   required_status_checks {
-    strict = true
+    strict   = true
     contexts = ["concourse-ci/status"]
   }
 
@@ -40,38 +40,38 @@ resource "github_branch_protection" "dataworks-aws-ucfs-stub_master" {
   }
 }
 
-resource "github_issue_label" "dataworks-aws-ucfs-stub" {
+resource "github_issue_label" "dataworks_aws_ucfs_stub" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
   name       = each.value.name
-  repository = github_repository.dataworks-aws-ucfs-stub.name
+  repository = github_repository.dataworks_aws_ucfs_stub.name
 }
 
-resource "null_resource" "dataworks-aws-ucfs-stub" {
+resource "null_resource" "dataworks_aws_ucfs_stub" {
   triggers = {
-    repo = github_repository.dataworks-aws-ucfs-stub.name
+    repo = github_repository.dataworks_aws_ucfs_stub.name
   }
   provisioner "local-exec" {
-    command = "./initial-commit.sh ${github_repository.dataworks-aws-ucfs-stub.name} '${github_repository.dataworks-aws-ucfs-stub.description}' ${github_repository.dataworks-aws-ucfs-stub.template.0.repository}"
+    command = "./initial-commit.sh ${github_repository.dataworks_aws_ucfs_stub.name} '${github_repository.dataworks_aws_ucfs_stub.description}' ${github_repository.dataworks_aws_ucfs_stub.template.0.repository}"
   }
 }
 
-resource "github_repository_webhook" "dataworks-aws-ucfs-stub" {
-  repository = github_repository.dataworks-aws-ucfs-stub.name
+resource "github_repository_webhook" "dataworks_aws_ucfs_stub" {
+  repository = github_repository.dataworks_aws_ucfs_stub.name
   events     = ["push"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.dataworks-aws-ucfs-stub.name}/resources/${github_repository.dataworks-aws-ucfs-stub.name}/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.dataworks_aws_ucfs_stub.name}/resources/${github_repository.dataworks_aws_ucfs_stub.name}/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
 
-resource "github_repository_webhook" "dataworks-aws-ucfs-stub_pr" {
-  repository = github_repository.dataworks-aws-ucfs-stub.name
+resource "github_repository_webhook" "dataworks_aws_ucfs_stub_pr" {
+  repository = github_repository.dataworks_aws_ucfs_stub.name
   events     = ["pull_request"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.dataworks-aws-ucfs-stub.name}/resources/${github_repository.dataworks-aws-ucfs-stub.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.dataworks_aws_ucfs_stub.name}/resources/${github_repository.dataworks_aws_ucfs_stub.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
