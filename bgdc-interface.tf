881c6f081e47a9f19a75a7ed44aed615c5662a5a
Dmitrijs Kovalenko <dmitrjiskovalenko@digital.uc.dwp.gov.uk>
2020-11-06 16:05:57 +0000

    rename

--- bgdc-interface.tf
+++ bgdc-interface.tf
@@ -1,6 +1,6 @@
-resource "github_repository" "dataworks_bgdc_interface" {
-  name             = "dataworks-bgdc-interface"
-  description      = "Infrastructure for the interface between BGDC and Dataworks"
+resource "github_repository" "dataworks_aws_bgdc_connectivity" {
+  name             = "dataworks-aws-bgdc-connectivity"
+  description      = "Infrastructure for the connectivity between BGDC and Dataworks"
   auto_init        = false
 
   allow_merge_commit     = false
@@ -18,15 +18,15 @@ resource "github_repository" "dataworks_bgdc_interface" {
   }
 }
 
-resource "github_team_repository" "dataworks_bgdc_interface_dataworks" {
-  repository = github_repository.dataworks_bgdc_interface.name
+resource "github_team_repository" "dataworks_aws_bgdc_connectivity_dataworks" {
+  repository = github_repository.dataworks_aws_bgdc_connectivity.name
   team_id    = github_team.dataworks.id
   permission = "push"
 }
 
-resource "github_branch_protection" "dataworks_bgdc_interface_master" {
-  branch         = github_repository.dataworks_bgdc_interface.default_branch
-  repository     = github_repository.dataworks_bgdc_interface.name
+resource "github_branch_protection" "dataworks_aws_bgdc_connectivity_master" {
+  branch         = github_repository.dataworks_aws_bgdc_connectivity.default_branch
+  repository     = github_repository.dataworks_aws_bgdc_connectivity.name
   enforce_admins = false
 
   required_status_checks {
@@ -40,38 +40,38 @@ resource "github_branch_protection" "dataworks_bgdc_interface_master" {
   }
 }
 
-resource "github_issue_label" "dataworks_bgdc_interface" {
+resource "github_issue_label" "dataworks_aws_bgdc_connectivity" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
   name       = each.value.name
-  repository = github_repository.dataworks_bgdc_interface.name
+  repository = github_repository.dataworks_aws_bgdc_connectivity.name
 }
 
-resource "null_resource" "dataworks_bgdc_interface" {
+resource "null_resource" "dataworks_aws_bgdc_connectivity" {
   triggers = {
-    repo = github_repository.dataworks_bgdc_interface.name
+    repo = github_repository.dataworks_aws_bgdc_connectivity.name
   }
   provisioner "local-exec" {
-    command = "./initial-commit.sh ${github_repository.dataworks_bgdc_interface.name} '${github_repository.dataworks_bgdc_interface.description}' ${github_repository.dataworks_bgdc_interface.template.0.repository}"
+    command = "./initial-commit.sh ${github_repository.dataworks_aws_bgdc_connectivity.name} '${github_repository.dataworks_aws_bgdc_connectivity.description}' ${github_repository.dataworks_aws_bgdc_connectivity.template.0.repository}"
   }
 }
 
-resource "github_repository_webhook" "dataworks_bgdc_interface" {
-  repository = github_repository.dataworks_bgdc_interface.name
+resource "github_repository_webhook" "dataworks_aws_bgdc_connectivity" {
+  repository = github_repository.dataworks_aws_bgdc_connectivity.name
   events     = ["push"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.dataworks_bgdc_interface.name}/resources/${github_repository.dataworks_bgdc_interface.name}/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.dataworks_aws_bgdc_connectivity.name}/resources/${github_repository.dataworks_aws_bgdc_connectivity.name}/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
 
-resource "github_repository_webhook" "dataworks_bgdc_interface_pr" {
-  repository = github_repository.dataworks_bgdc_interface.name
+resource "github_repository_webhook" "dataworks_aws_bgdc_connectivity_pr" {
+  repository = github_repository.dataworks_aws_bgdc_connectivity.name
   events     = ["pull_request"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.dataworks_bgdc_interface.name}/resources/${github_repository.dataworks_bgdc_interface.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.dataworks_aws_bgdc_connectivity.name}/resources/${github_repository.dataworks_aws_bgdc_connectivity.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
