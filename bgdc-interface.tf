72236a31cbd92cbed22f9de6e5843f3db9e158e6
Dmitrijs Kovalenko <dmitrjiskovalenko@digital.uc.dwp.gov.uk>
2020-11-06 15:36:39 +0000

    rename properly

--- bgdc-interface.tf
+++ bgdc-interface.tf
@@ -1,4 +1,4 @@
-resource "github_repository" "bgdc_interface" {
+resource "github_repository" "dataworks_bgdc_interface" {
   name             = "dataworks-bgdc-interface"
   description      = "Infrastructure for the interface between BGDC and Dataworks"
   auto_init        = false
@@ -18,15 +18,15 @@ resource "github_repository" "bgdc_interface" {
   }
 }
 
-resource "github_team_repository" "bgdc_interface_dataworks" {
-  repository = github_repository.bgdc_interface.name
+resource "github_team_repository" "dataworks_bgdc_interface_dataworks" {
+  repository = github_repository.dataworks_bgdc_interface.name
   team_id    = github_team.dataworks.id
   permission = "push"
 }
 
-resource "github_branch_protection" "bgdc_interface_master" {
-  branch         = github_repository.bgdc_interface.default_branch
-  repository     = github_repository.bgdc_interface.name
+resource "github_branch_protection" "dataworks_bgdc_interface_master" {
+  branch         = github_repository.dataworks_bgdc_interface.default_branch
+  repository     = github_repository.dataworks_bgdc_interface.name
   enforce_admins = false
 
   required_status_checks {
@@ -40,38 +40,38 @@ resource "github_branch_protection" "bgdc_interface_master" {
   }
 }
 
-resource "github_issue_label" "bgdc_interface" {
+resource "github_issue_label" "dataworks_bgdc_interface" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
   name       = each.value.name
-  repository = github_repository.bgdc_interface.name
+  repository = github_repository.dataworks_bgdc_interface.name
 }
 
-resource "null_resource" "bgdc_interface" {
+resource "null_resource" "dataworks_bgdc_interface" {
   triggers = {
-    repo = github_repository.bgdc_interface.name
+    repo = github_repository.dataworks_bgdc_interface.name
   }
   provisioner "local-exec" {
-    command = "./initial-commit.sh ${github_repository.bgdc_interface.name} '${github_repository.bgdc_interface.description}' ${github_repository.bgdc_interface.template.0.repository}"
+    command = "./initial-commit.sh ${github_repository.dataworks_bgdc_interface.name} '${github_repository.dataworks_bgdc_interface.description}' ${github_repository.dataworks_bgdc_interface.template.0.repository}"
   }
 }
 
-resource "github_repository_webhook" "bgdc_interface" {
-  repository = github_repository.bgdc_interface.name
+resource "github_repository_webhook" "dataworks_bgdc_interface" {
+  repository = github_repository.dataworks_bgdc_interface.name
   events     = ["push"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.bgdc_interface.name}/resources/${github_repository.bgdc_interface.name}/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.dataworks_bgdc_interface.name}/resources/${github_repository.dataworks_bgdc_interface.name}/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
 
-resource "github_repository_webhook" "bgdc_interface_pr" {
-  repository = github_repository.bgdc_interface.name
+resource "github_repository_webhook" "dataworks_bgdc_interface_pr" {
+  repository = github_repository.dataworks_bgdc_interface.name
   events     = ["pull_request"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.bgdc_interface.name}/resources/${github_repository.bgdc_interface.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.dataworks_bgdc_interface.name}/resources/${github_repository.dataworks_bgdc_interface.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
