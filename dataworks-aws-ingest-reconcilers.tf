36b5092b8db5fb129217188327edbaf356e1b071
mark matthews <markmatthews@digital.uc.dwp.gov.uk>
2020-11-30 14:30:57 +0000

    set up new repo

--- dataworks-aws-ingest-reconcilers.tf
+++ dataworks-aws-ingest-reconcilers.tf
@@ -1,6 +1,6 @@
-resource "github_repository" "example" {
-  name             = "example"
-  description      = "example"
+resource "github_repository" "dataworks_aws_ingest_reconcilers" {
+  name             = "dataworks-aws-ingest-reconcilers"
+  description      = "Manages the reconcilers and trimmers for aws ingestion feeds"
   auto_init        = false
 
   allow_merge_commit     = false
@@ -18,15 +18,15 @@ resource "github_repository" "example" {
   }
 }
 
-resource "github_team_repository" "example_dataworks" {
-  repository = github_repository.example.name
+resource "github_team_repository" "dataworks-aws-ingest-reconcilers_dataworks" {
+  repository = github_repository.dataworks_aws_ingest_reconcilers.name
   team_id    = github_team.dataworks.id
   permission = "push"
 }
 
-resource "github_branch_protection" "example_master" {
-  branch         = github_repository.example.default_branch
-  repository     = github_repository.example.name
+resource "github_branch_protection" "dataworks-aws-ingest-reconcilers_master" {
+  branch         = github_repository.dataworks_aws_ingest_reconcilers.default_branch
+  repository     = github_repository.dataworks_aws_ingest_reconcilers.name
   enforce_admins = false
 
   required_status_checks {
@@ -40,38 +40,38 @@ resource "github_branch_protection" "example_master" {
   }
 }
 
-resource "github_issue_label" "example" {
+resource "github_issue_label" "dataworks-aws-ingest-reconcilers" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
   name       = each.value.name
-  repository = github_repository.example.name
+  repository = github_repository.dataworks_aws_ingest_reconcilers.name
 }
 
-resource "null_resource" "example" {
+resource "null_resource" "dataworks-aws-ingest-reconcilers" {
   triggers = {
-    repo = github_repository.example.name
+    repo = github_repository.dataworks_aws_ingest_reconcilers.name
   }
   provisioner "local-exec" {
-    command = "./initial-commit.sh ${github_repository.example.name} '${github_repository.example.description}' ${github_repository.example.template.0.repository}"
+    command = "./initial-commit.sh ${github_repository.dataworks_aws_ingest_reconcilers.name} '${github_repository.dataworks_aws_ingest_reconcilers.description}' ${github_repository.dataworks_aws_ingest_reconcilers.template.0.repository}"
   }
 }
 
-resource "github_repository_webhook" "example" {
-  repository = github_repository.example.name
+resource "github_repository_webhook" "dataworks-aws-ingest-reconcilers" {
+  repository = github_repository.dataworks_aws_ingest_reconcilers.name
   events     = ["push"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.example.name}/resources/${github_repository.example.name}/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.dataworks_aws_ingest_reconcilers.name}/resources/${github_repository.dataworks_aws_ingest_reconcilers.name}/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
 
-resource "github_repository_webhook" "example_pr" {
-  repository = github_repository.example.name
+resource "github_repository_webhook" "dataworks-aws-ingest-reconcilers_pr" {
+  repository = github_repository.dataworks_aws_ingest_reconcilers.name
   events     = ["pull_request"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.example.name}/resources/${github_repository.example.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${github_repository.dataworks_aws_ingest_reconcilers.name}/resources/${github_repository.dataworks_aws_ingest_reconcilers.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
