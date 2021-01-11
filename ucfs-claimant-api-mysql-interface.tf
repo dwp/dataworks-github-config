0a0a947e6f243c5ce08cfef68ba8c9a44ce5c27b
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-01-11 11:48:52 +0000

    Correct secret names

--- ucfs-claimant-api-mysql-interface.tf
+++ ucfs-claimant-api-mysql-interface.tf
@@ -57,18 +57,18 @@ resource "null_resource" "ucfs_claimant_api_mysql_interface" {
 
 resource "github_actions_secret" "ucfs-claimant-api-mysql-interface_github_email" {
   repository      = github_repository.ucfs_claimant_api_mysql_interface.name
-  secret_name     = "GITHUB_EMAIL"
+  secret_name     = "CI_GITHUB_EMAIL"
   plaintext_value = var.github_email
 }
 
 resource "github_actions_secret" "ucfs-claimant-api-mysql-interface_github_username" {
   repository      = github_repository.ucfs_claimant_api_mysql_interface.name
-  secret_name     = "GITHUB_USERNAME"
+  secret_name     = "CI_GITHUB_USERNAME"
   plaintext_value = var.github_username
 }
 
 resource "github_actions_secret" "ucfs-claimant-api-mysql-interface_github_token" {
   repository      = github_repository.ucfs_claimant_api_mysql_interface.name
-  secret_name     = "GITHUB_TOKEN"
+  secret_name     = "CI_GITHUB_TOKEN"
   plaintext_value = var.github_token
 }
