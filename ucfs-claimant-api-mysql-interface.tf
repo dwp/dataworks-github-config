8bff5cb8b706578c7a6b4aec9a1b75158a0f852c
Connor Avery <55276424+connorIW@users.noreply.github.com>
2021-01-11 11:58:51 +0000

    Fix ucfs claimant lambda (#279)
    * Include secrets required for GHA

--- ucfs-claimant-api-mysql-interface.tf
+++ ucfs-claimant-api-mysql-interface.tf
@@ -54,3 +54,15 @@ resource "null_resource" "ucfs_claimant_api_mysql_interface" {
     command = "./initial-commit.sh ${github_repository.ucfs_claimant_api_mysql_interface.name} '${github_repository.ucfs_claimant_api_mysql_interface.description}' ${github_repository.ucfs_claimant_api_mysql_interface.template.0.repository}"
   }
 }
+
+resource "github_actions_secret" "ucfs_claimant_api_mysql_interface_github_email" {
+  repository      = github_repository.ucfs_claimant_api_mysql_interface.name
+  secret_name     = "CI_GITHUB_EMAIL"
+  plaintext_value = var.github_email
+}
+
+resource "github_actions_secret" "ucfs_claimant_api_mysql_interface_github_username" {
+  repository      = github_repository.ucfs_claimant_api_mysql_interface.name
+  secret_name     = "CI_GITHUB_USERNAME"
+  plaintext_value = var.github_username
+}
