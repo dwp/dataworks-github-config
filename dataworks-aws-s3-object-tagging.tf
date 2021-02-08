894dc618f07bad56ac60cc5b6e62f7ff3ae8c095
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-02-08 10:07:33 +0000

    tagger
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- dataworks-aws-s3-object-tagging.tf
+++ dataworks-aws-s3-object-tagging.tf
@@ -1,10 +1,10 @@
 locals {
-  dataworks_aws_s3_object_tagging_pipeline_name = "s3-object-tagging"
+  dataworks_aws_s3_object_tagger_pipeline_name = "s3-object-tagger"
 }
 
-resource "github_repository" "dataworks_aws_s3_object_tagging" {
-  name             = "dataworks-aws-s3-object-tagging"
-  description      = "S3 Object Tagging application infrastructure"
+resource "github_repository" "dataworks_aws_s3_object_tagger" {
+  name             = "dataworks-aws-s3-object-tagger"
+  description      = "S3 Object tagger application infrastructure"
   auto_init        = false
 
   allow_merge_commit     = false
@@ -22,15 +22,15 @@ resource "github_repository" "dataworks_aws_s3_object_tagging" {
   }
 }
 
-resource "github_team_repository" "dataworks_aws_s3_object_tagging_dataworks" {
-  repository = github_repository.dataworks_aws_s3_object_tagging.name
+resource "github_team_repository" "dataworks_aws_s3_object_tagger_dataworks" {
+  repository = github_repository.dataworks_aws_s3_object_tagger.name
   team_id    = github_team.dataworks.id
   permission = "push"
 }
 
-resource "github_branch_protection" "dataworks_aws_s3_object_tagging_master" {
-  branch         = github_repository.dataworks_aws_s3_object_tagging.default_branch
-  repository     = github_repository.dataworks_aws_s3_object_tagging.name
+resource "github_branch_protection" "dataworks_aws_s3_object_tagger_master" {
+  branch         = github_repository.dataworks_aws_s3_object_tagger.default_branch
+  repository     = github_repository.dataworks_aws_s3_object_tagger.name
   enforce_admins = false
 
   required_status_checks {
@@ -44,38 +44,38 @@ resource "github_branch_protection" "dataworks_aws_s3_object_tagging_master" {
   }
 }
 
-resource "github_issue_label" "dataworks_aws_s3_object_tagging" {
+resource "github_issue_label" "dataworks_aws_s3_object_tagger" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
   name       = each.value.name
-  repository = github_repository.dataworks_aws_s3_object_tagging.name
+  repository = github_repository.dataworks_aws_s3_object_tagger.name
 }
 
-resource "null_resource" "dataworks_aws_s3_object_tagging" {
+resource "null_resource" "dataworks_aws_s3_object_tagger" {
   triggers = {
-    repo = github_repository.dataworks_aws_s3_object_tagging.name
+    repo = github_repository.dataworks_aws_s3_object_tagger.name
   }
   provisioner "local-exec" {
-    command = "./initial-commit.sh ${github_repository.dataworks_aws_s3_object_tagging.name} '${github_repository.dataworks_aws_s3_object_tagging.description}' ${github_repository.dataworks_aws_s3_object_tagging.template.0.repository}"
+    command = "./initial-commit.sh ${github_repository.dataworks_aws_s3_object_tagger.name} '${github_repository.dataworks_aws_s3_object_tagger.description}' ${github_repository.dataworks_aws_s3_object_tagger.template.0.repository}"
   }
 }
 
-resource "github_repository_webhook" "dataworks_aws_s3_object_tagging" {
-  repository = github_repository.dataworks_aws_s3_object_tagging.name
+resource "github_repository_webhook" "dataworks_aws_s3_object_tagger" {
+  repository = github_repository.dataworks_aws_s3_object_tagger.name
   events     = ["push"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.dataworks_aws_s3_object_tagging_pipeline_name}/resources/${github_repository.dataworks_aws_s3_object_tagging.name}/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.dataworks_aws_s3_object_tagger_pipeline_name}/resources/${github_repository.dataworks_aws_s3_object_tagger.name}/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
 
-resource "github_repository_webhook" "dataworks_aws_s3_object_tagging_pr" {
-  repository = github_repository.dataworks_aws_s3_object_tagging.name
+resource "github_repository_webhook" "dataworks_aws_s3_object_tagger_pr" {
+  repository = github_repository.dataworks_aws_s3_object_tagger.name
   events     = ["pull_request"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.dataworks_aws_s3_object_tagging_pipeline_name}/resources/${github_repository.dataworks_aws_s3_object_tagging.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.dataworks_aws_s3_object_tagger_pipeline_name}/resources/${github_repository.dataworks_aws_s3_object_tagger.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
