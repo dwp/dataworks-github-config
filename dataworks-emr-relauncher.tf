6edb75f06997bc9a5f63e873a22d40749dc74c8d
Mark <mark.stevens@infinityworks.com>
2021-02-19 09:49:26 +0000

    changed terraform resource names to be underscores
    Signed-off-by: Mark <mark.stevens@infinityworks.com>

--- dataworks-emr-relauncher.tf
+++ dataworks-emr-relauncher.tf
@@ -1,4 +1,4 @@
-resource "github_repository" "dataworks-emr-relauncher" {
+resource "github_repository" "dataworks_emr_relauncher" {
   name        = "dataworks-emr-relauncher"
   description = "Lambda based EMR Cluster relauncher"
   auto_init   = true
@@ -13,15 +13,15 @@ resource "github_repository" "dataworks-emr-relauncher" {
   }
 }
 
-resource "github_team_repository" "dataworks-emr-relauncher-dataworks" {
-  repository = github_repository.dataworks-emr-relauncher.name
+resource "github_team_repository" "dataworks_emr_relauncher_dataworks" {
+  repository = github_repository.dataworks_emr_relauncher.name
   team_id    = github_team.dataworks.id
   permission = "push"
 }
 
-resource "github_branch_protection" "dataworks-emr-relauncher-master" {
-  branch         = github_repository.dataworks-emr-relauncher.default_branch
-  repository     = github_repository.dataworks-emr-relauncher.name
+resource "github_branch_protection" "dataworks_emr_relauncher_master" {
+  branch         = github_repository.dataworks_emr_relauncher.default_branch
+  repository     = github_repository.dataworks_emr_relauncher.name
   enforce_admins = false
 
   required_status_checks {
@@ -35,27 +35,27 @@ resource "github_branch_protection" "dataworks-emr-relauncher-master" {
   }
 }
 
-resource "github_issue_label" "dataworks-emr-relauncher" {
+resource "github_issue_label" "dataworks_emr_relauncher" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
   name       = each.value.name
-  repository = github_repository.dataworks-emr-relauncher.name
+  repository = github_repository.dataworks_emr_relauncher.name
 }
 
-resource "github_actions_secret" "dataworks-emr-relauncher-github-email" {
-  repository      = github_repository.dataworks-emr-relauncher.name
+resource "github_actions_secret" "dataworks_emr_relauncher_github_email" {
+  repository      = github_repository.dataworks_emr_relauncher.name
   secret_name     = "CI_GITHUB_EMAIL"
   plaintext_value = var.github_email
 }
 
-resource "github_actions_secret" "dataworks-emr-relauncher-github-username" {
-  repository      = github_repository.dataworks-emr-relauncher.name
+resource "github_actions_secret" "dataworks_emr_relauncher_github_username" {
+  repository      = github_repository.dataworks_emr_relauncher.name
   secret_name     = "CI_GITHUB_USERNAME"
   plaintext_value = var.github_username
 }
 
-resource "github_actions_secret" "dataworks-emr-relauncher-github-token" {
-  repository      = github_repository.dataworks-emr-relauncher.name
+resource "github_actions_secret" "dataworks_emr_relauncher_github_token" {
+  repository      = github_repository.dataworks_emr_relauncher.name
   secret_name     = "CI_GITHUB_TOKEN"
   plaintext_value = var.github_token
 }
