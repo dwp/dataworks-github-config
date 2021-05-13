8aecc783fd72b4974f31c8256e267ef0df3aeb51
vipinchadha <vipin.chadha@engineering.digital.dwp.gov.uk>
2021-05-13 11:40:28 +0100

    Renamed local variable to avoid duplication

--- dataworks-ml-streams-infrastructure.tf
+++ dataworks-ml-streams-infrastructure.tf
@@ -1,5 +1,5 @@
 locals {
-  pipeline_name = "dataworks_ml_streams_infrastructure"
+  dataworks_ml_streams_infrastructure_pipeline_name = "dataworks_ml_streams_infrastructure"
 }
 
 resource "github_repository" "dataworks_ml_streams_infrastructure" {
@@ -65,7 +65,7 @@ resource "github_repository_webhook" "dataworks_ml_streams_infrastructure" {
   events     = ["push"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.pipeline_name}/resources/${github_repository.dataworks_ml_streams_infrastructure.name}/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.dataworks_ml_streams_infrastructure_pipeline_name}/resources/${github_repository.dataworks_ml_streams_infrastructure.name}/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
@@ -75,7 +75,7 @@ resource "github_repository_webhook" "dataworks_ml_streams_infrastructure_pr" {
   events     = ["pull_request"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.pipeline_name}/resources/${github_repository.dataworks_ml_streams_infrastructure.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.dataworks_ml_streams_infrastructure_pipeline_name}/resources/${github_repository.dataworks_ml_streams_infrastructure.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
