df73a5fe3731da7dd9ea1e461586a57284270374
vipinchadha <vipin.chadha@engineering.digital.dwp.gov.uk>
2021-05-13 11:24:07 +0100

    Applying review comments

--- dataworks-ml-streams-infrastructure.tf
+++ dataworks-ml-streams-infrastructure.tf
@@ -1,9 +1,9 @@
 locals {
-  pipeline_name = "dataworks-ml-streams-broker"
+  pipeline_name = "dataworks_ml_streams_infrastructure"
 }
 
-resource "github_repository" "dataworks-ml-streams-broker" {
-  name             = "dataworks-ml-streams-broker"
+resource "github_repository" "dataworks_ml_streams_infrastructure" {
+  name             = "dataworks-ml-streams-infrastructure"
   description      = "Dataworks repository for provisioning kafka cluster"
   auto_init        = false
 
@@ -22,15 +22,15 @@ resource "github_repository" "dataworks-ml-streams-broker" {
   }
 }
 
-resource "github_team_repository" "dataworks-ml-streams-broker" {
-  repository = github_repository.dataworks-ml-streams-broker.name
+resource "github_team_repository" "dataworks_ml_streams_infrastructure" {
+  repository = github_repository.dataworks_ml_streams_infrastructure.name
   team_id    = github_team.dataworks.id
   permission = "push"
 }
 
-resource "github_branch_protection" "dataworks-ml-streams-broker-master" {
-  branch         = github_repository.dataworks-ml-streams-broker.default_branch
-  repository     = github_repository.dataworks-ml-streams-broker.name
+resource "github_branch_protection" "dataworks_ml_streams_infrastructure_master" {
+  branch         = github_repository.dataworks_ml_streams_infrastructure.default_branch
+  repository     = github_repository.dataworks_ml_streams_infrastructure.name
   enforce_admins = true
 
   required_status_checks {
@@ -44,38 +44,38 @@ resource "github_branch_protection" "dataworks-ml-streams-broker-master" {
   }
 }
 
-resource "github_issue_label" "dataworks-ml-streams-broker" {
+resource "github_issue_label" "dataworks_ml_streams_infrastructure" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
   name       = each.value.name
-  repository = github_repository.dataworks-ml-streams-broker.name
+  repository = github_repository.dataworks_ml_streams_infrastructure.name
 }
 
-resource "null_resource" "dataworks-ml-streams-broker" {
+resource "null_resource" "dataworks_ml_streams_infrastructure" {
   triggers = {
-    repo = github_repository.dataworks-ml-streams-broker.name
+    repo = github_repository.dataworks_ml_streams_infrastructure.name
   }
   provisioner "local-exec" {
-    command = "./initial-commit.sh ${github_repository.dataworks-ml-streams-broker.name} '${github_repository.dataworks-ml-streams-broker.description}' ${github_repository.dataworks-ml-streams-broker.template.0.repository}"
+    command = "./initial-commit.sh ${github_repository.dataworks_ml_streams_infrastructure.name} '${github_repository.dataworks_ml_streams_infrastructure.description}' ${github_repository.dataworks_ml_streams_infrastructure.template.0.repository}"
   }
 }
 
-resource "github_repository_webhook" "dataworks-ml-streams-broker" {
-  repository = github_repository.dataworks-ml-streams-broker.name
+resource "github_repository_webhook" "dataworks_ml_streams_infrastructure" {
+  repository = github_repository.dataworks_ml_streams_infrastructure.name
   events     = ["push"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.pipeline_name}/resources/${github_repository.dataworks-ml-streams-broker.name}/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.pipeline_name}/resources/${github_repository.dataworks_ml_streams_infrastructure.name}/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
 
-resource "github_repository_webhook" "dataworks-ml-streams-broker_pr" {
-  repository = github_repository.dataworks-ml-streams-broker.name
+resource "github_repository_webhook" "dataworks_ml_streams_infrastructure_pr" {
+  repository = github_repository.dataworks_ml_streams_infrastructure.name
   events     = ["pull_request"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.pipeline_name}/resources/${github_repository.dataworks-ml-streams-broker.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.pipeline_name}/resources/${github_repository.dataworks_ml_streams_infrastructure.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
