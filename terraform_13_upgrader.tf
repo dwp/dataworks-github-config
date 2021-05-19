2e26a83c4ba6160992a65e1fe025a9bf15150183
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-05-19 16:48:47 +0100

    Generic name
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- terraform_13_upgrader.tf
+++ terraform_13_upgrader.tf
@@ -1,6 +1,6 @@
-resource "github_repository" "terraform_13_upgrader" {
-  name             = "terraform-13-upgrader"
-  description      = "A suite of tools to facilitate the upgrading of terraform to version 0.13 across many repos."
+resource "github_repository" "terraform_upgrader" {
+  name             = "terraform_upgrader"
+  description      = "A suite of tools to facilitate the upgrading of terraform across many repos."
   auto_init        = false
 
   allow_merge_commit     = false
@@ -18,15 +18,15 @@ resource "github_repository" "terraform_13_upgrader" {
   }
 }
 
-resource "github_team_repository" "terraform_13_upgrader_dataworks" {
-  repository = github_repository.terraform_13_upgrader.name
+resource "github_team_repository" "terraform_upgrader_dataworks" {
+  repository = github_repository.terraform_upgrader.name
   team_id    = github_team.dataworks.id
   permission = "push"
 }
 
-resource "github_branch_protection" "terraform_13_upgrader_master" {
-  branch         = github_repository.terraform_13_upgrader.default_branch
-  repository     = github_repository.terraform_13_upgrader.name
+resource "github_branch_protection" "terraform_upgrader_master" {
+  branch         = github_repository.terraform_upgrader.default_branch
+  repository     = github_repository.terraform_upgrader.name
   enforce_admins = true
 
   required_status_checks {
@@ -40,36 +40,36 @@ resource "github_branch_protection" "terraform_13_upgrader_master" {
   }
 }
 
-resource "github_issue_label" "terraform_13_upgrader" {
+resource "github_issue_label" "terraform_upgrader" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
   name       = each.value.name
-  repository = github_repository.terraform_13_upgrader.name
+  repository = github_repository.terraform_upgrader.name
 }
 
-resource "null_resource" "terraform_13_upgrader" {
+resource "null_resource" "terraform_upgrader" {
   triggers = {
-    repo = github_repository.terraform_13_upgrader.name
+    repo = github_repository.terraform_upgrader.name
   }
   provisioner "local-exec" {
-    command = "./initial-commit.sh ${github_repository.terraform_13_upgrader.name} '${github_repository.terraform_13_upgrader.description}' ${github_repository.terraform_13_upgrader.template.0.repository}"
+    command = "./initial-commit.sh ${github_repository.terraform_upgrader.name} '${github_repository.terraform_upgrader.description}' ${github_repository.terraform_upgrader.template.0.repository}"
   }
 }
 
-resource "github_actions_secret" "terraform_13_upgrader_dockerhub_password" {
-  repository      = github_repository.terraform_13_upgrader.name
+resource "github_actions_secret" "terraform_upgrader_dockerhub_password" {
+  repository      = github_repository.terraform_upgrader.name
   secret_name     = "DOCKERHUB_PASSWORD"
   plaintext_value = var.dockerhub_password
 }
 
-resource "github_actions_secret" "terraform_13_upgrader_dockerhub_username" {
-  repository      = github_repository.terraform_13_upgrader.name
+resource "github_actions_secret" "terraform_upgrader_dockerhub_username" {
+  repository      = github_repository.terraform_upgrader.name
   secret_name     = "DOCKERHUB_USERNAME"
   plaintext_value = var.dockerhub_username
 }
 
-resource "github_actions_secret" "terraform_13_upgrader_snyk_token" {
-  repository      = github_repository.terraform_13_upgrader.name
+resource "github_actions_secret" "terraform_upgrader_snyk_token" {
+  repository      = github_repository.terraform_upgrader.name
   secret_name     = "SNYK_TOKEN"
   plaintext_value = var.snyk_token
 }
