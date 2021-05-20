b39b8b95c45f81961319b9d32dd752d025b64dff
Zain Daniyal <zain.daniyal@engineering.digital.dwp.gov.uk>
2021-05-20 15:28:04 +0100

    made changes to prefix. changed to dataworks-aws-sagemaker

--- dataworks-sagemaker-infrastructure.tf
+++ dataworks-sagemaker-infrastructure.tf
@@ -1,9 +1,9 @@
 locals {
-  dataworks_sagemaker_infrastructure_pipeline_name = "dataworks_sagemaker_infrastructure"
+  dataworks_aws_sagemaker_infrastructure_pipeline_name = "dataworks_aws_sagemaker_infrastructure"
 }
 
-resource "github_repository" "dataworks_sagemaker_infrastructure" {
-  name             = "dataworks-sagemaker-infrastructure"
+resource "github_repository" "dataworks_aws_sagemaker_infrastructure" {
+  name             = "dataworks-aws-sagemaker-infrastructure"
   description      = "Dataworks repository for provisioning sagemaker"
   auto_init        = false
 
@@ -22,15 +22,15 @@ resource "github_repository" "dataworks_sagemaker_infrastructure" {
   }
 }
 
-resource "github_team_repository" "dataworks_sagemaker_infrastructure" {
-  repository = github_repository.dataworks_sagemaker_infrastructure.name
+resource "github_team_repository" "dataworks_aws_sagemaker_infrastructure" {
+  repository = github_repository.dataworks_aws_sagemaker_infrastructure.name
   team_id    = github_team.dataworks.id
   permission = "push"
 }
 
-resource "github_branch_protection" "dataworks_sagemaker_infrastructure_master" {
-  branch         = github_repository.dataworks_sagemaker_infrastructure.default_branch
-  repository     = github_repository.dataworks_sagemaker_infrastructure.name
+resource "github_branch_protection" "dataworks_aws_sagemaker_infrastructure_master" {
+  branch         = github_repository.dataworks_aws_sagemaker_infrastructure.default_branch
+  repository     = github_repository.dataworks_aws_sagemaker_infrastructure.name
   enforce_admins = true
 
   required_status_checks {
@@ -44,38 +44,38 @@ resource "github_branch_protection" "dataworks_sagemaker_infrastructure_master" 
   }
 }
 
-resource "github_issue_label" "dataworks_sagemaker_infrastructure" {
+resource "github_issue_label" "dataworks_aws_sagemaker_infrastructure" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
   name       = each.value.name
-  repository = github_repository.dataworks_sagemaker_infrastructure.name
+  repository = github_repository.dataworks_aws_sagemaker_infrastructure.name
 }
 
-resource "null_resource" "dataworks_sagemaker_infrastructure" {
+resource "null_resource" "dataworks_aws_sagemaker_infrastructure" {
   triggers = {
-    repo = github_repository.dataworks_sagemaker_infrastructure.name
+    repo = github_repository.dataworks_aws_sagemaker_infrastructure.name
   }
   provisioner "local-exec" {
-    command = "./initial-commit.sh ${github_repository.dataworks_sagemaker_infrastructure.name} '${github_repository.dataworks_sagemaker_infrastructure.description}' ${github_repository.dataworks_sagemaker_infrastructure.template.0.repository}"
+    command = "./initial-commit.sh ${github_repository.dataworks_aws_sagemaker_infrastructure.name} '${github_repository.dataworks_aws_sagemaker_infrastructure.description}' ${github_repository.dataworks_aws_sagemaker_infrastructure.template.0.repository}"
   }
 }
 
-resource "github_repository_webhook" "dataworks_sagemaker_infrastructure" {
-  repository = github_repository.dataworks_sagemaker_infrastructure.name
+resource "github_repository_webhook" "dataworks_aws_sagemaker_infrastructure" {
+  repository = github_repository.dataworks_aws_sagemaker_infrastructure.name
   events     = ["push"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.dataworks_sagemaker_infrastructure_pipeline_name}/resources/${github_repository.dataworks_sagemaker_infrastructure.name}/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.dataworks_aws_sagemaker_infrastructure_pipeline_name}/resources/${github_repository.dataworks_aws_sagemaker_infrastructure.name}/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
 
-resource "github_repository_webhook" "dataworks_sagemaker_infrastructure_pr" {
-  repository = github_repository.dataworks_sagemaker_infrastructure.name
+resource "github_repository_webhook" "dataworks_aws_sagemaker_infrastructure_pr" {
+  repository = github_repository.dataworks_aws_sagemaker_infrastructure.name
   events     = ["pull_request"]
 
   configuration {
-    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.dataworks_sagemaker_infrastructure_pipeline_name}/resources/${github_repository.dataworks_sagemaker_infrastructure.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.dataworks_aws_sagemaker_infrastructure_pipeline_name}/resources/${github_repository.dataworks_aws_sagemaker_infrastructure.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
     content_type = "form"
   }
 }
