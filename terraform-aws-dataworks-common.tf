b6e2a159c0330f3c1978dc6d94e977e837cc99da
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-05-21 08:46:43 +0100

    Underscores

--- terraform-aws-dataworks-common.tf
+++ terraform-aws-dataworks-common.tf
@@ -14,14 +14,14 @@ resource "github_repository" "terraform_aws_dataworks_common" {
 }
 
 resource "github_team_repository" "terraform_aws_dataworks_common_dataworks" {
-  repository = github_repository.terraform_aws_dataworks_common_.name
+  repository = github_repository.terraform_aws_dataworks_common.name
   team_id    = github_team.dataworks.id
   permission = "push"
 }
 
 resource "github_branch_protection" "terraform_aws_dataworks_common_master" {
-  branch         = github_repository.terraform_aws_dataworks_common_.default_branch
-  repository     = github_repository.terraform_aws_dataworks_common_.name
+  branch         = github_repository.terraform_aws_dataworks_common.default_branch
+  repository     = github_repository.terraform_aws_dataworks_common.name
   enforce_admins = true
 
   required_status_checks {
@@ -38,6 +38,6 @@ resource "github_issue_label" "terraform_aws_dataworks_common_" {
   for_each   = { for common_label in local.common_labels : common_label.name => common_label }
   color      = each.value.colour
   name       = each.value.name
-  repository = github_repository.terraform_aws_dataworks_common_.name
+  repository = github_repository.terraform_aws_dataworks_common.name
 }
 
