d6a85d78e6909e54125efa1124994a90d72a101f
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-05-21 09:59:51 +0100

    Default is now main, so we shouldn't hardcode on new repos to master

--- terraform-aws-dataworks-common.tf
+++ terraform-aws-dataworks-common.tf
@@ -5,7 +5,6 @@ resource "github_repository" "terraform_aws_dataworks_common" {
 
   allow_merge_commit     = false
   delete_branch_on_merge = true
-  default_branch         = "master"
   has_issues             = true
   topics                 = concat(local.common_topics, local.aws_topics)
 
