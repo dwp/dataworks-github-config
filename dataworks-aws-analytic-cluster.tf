876b6fff4696d1ae11cb68cff3388310333b6abd
WW Teo <whay-wang.teo@engineering.digital.dwp.gov.uk>
2021-06-24 14:41:52 +0100

    create repo for aws analytic emr cluster

---
+++ dataworks-aws-analytic-cluster.tf
@@ -0,0 +1,81 @@
+locals {
+  aws_analytic_cluster_pipeline_name = "aws-analytic-cluster"
+}
+
+resource "github_repository" "aws_analytic_cluster" {
+  name             = "dataworks-aws-analytic-cluster"
+  description      = "The EMR infrastructure for sagemaker analytic"
+  auto_init        = false
+
+  allow_merge_commit     = false
+  delete_branch_on_merge = true
+  has_issues             = true
+  topics                 = concat(local.common_topics, local.aws_topics)
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  template {
+    owner = var.github_organization
+    repository = "aws-emr-template-repository"
+  }
+}
+
+resource "github_team_repository" "aws_analytic_cluster_dataworks" {
+  repository = github_repository.aws_analytic_cluster.name
+  team_id    = github_team.dataworks.id
+  permission = "push"
+}
+
+resource "github_branch_protection" "aws_analytic_cluster_master" {
+  branch         = github_repository.aws_analytic_cluster.default_branch
+  repository     = github_repository.aws_analytic_cluster.name
+  enforce_admins = false
+
+  required_status_checks {
+    strict = true
+    contexts = ["concourse-ci/status"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews      = true
+    require_code_owner_reviews = true
+  }
+}
+
+resource "github_issue_label" "aws_analytic_cluster" {
+  for_each   = { for common_label in local.common_labels : common_label.name => common_label }
+  color      = each.value.colour
+  name       = each.value.name
+  repository = github_repository.aws_analytic_cluster.name
+}
+
+resource "null_resource" "aws_analytic_cluster" {
+  triggers = {
+    repo = github_repository.aws_analytic_cluster.name
+  }
+  provisioner "local-exec" {
+    command = "./initial-commit.sh ${github_repository.aws_analytic_cluster.name} '${github_repository.aws_analytic_cluster.description}' ${github_repository.aws_analytic_cluster.template.0.repository}"
+  }
+}
+
+resource "github_repository_webhook" "aws_analytic_cluster" {
+  repository = github_repository.aws_analytic_cluster.name
+  events     = ["push"]
+
+  configuration {
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.aws_analytic_cluster_pipeline_name}/resources/${github_repository.aws_analytic_cluster.name}/check/webhook?webhook_token=${var.github_webhook_token}"
+    content_type = "form"
+  }
+}
+
+resource "github_repository_webhook" "aws_analytic_cluster_pr" {
+  repository = github_repository.aws_analytic_cluster.name
+  events     = ["pull_request"]
+
+  configuration {
+    url          = "https://${var.aws_concourse_domain_name}/api/v1/teams/${var.aws_concourse_team}/pipelines/${local.aws_analytic_cluster_pipeline_name}/resources/${github_repository.aws_analytic_cluster.name}-pr/check/webhook?webhook_token=${var.github_webhook_token}"
+    content_type = "form"
+  }
+}
